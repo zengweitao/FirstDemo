5d1d3130e8a8eb75a6eac5264231fc9fd26d34e6
zengweitao <1948743201@qq.com>
2016-09-30 17:32:47 +0800

    添加一个类

---
+++ app/src/main/java/com/example/administrator/github/MyView.java
@@ -0,0 +1,13 @@
+package com.example.administrator.github;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/9/30.
+ */
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+}
