e67c4d60704775e523fab32096f7e8ac076f584d
zengweitao <1948743201@qq.com>
2016-09-30 16:51:56 +0800

    11

---
+++ app/src/main/java/com/example/administrator/github/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.administrator.github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.sql.SQLOutput;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        System.out.println("----hahh ");
+    }
+}
